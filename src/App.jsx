85dcaf81e29ad3276b9e0fb52839630cd53d5751
Sam24 <aclian4321@gmail.com>
2026-05-08 03:23:46 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='font-sans text-blue-500 text-4xl text-center'>
+      hello world
+    </div>
+  )
+}
+
+export default App
